e71f05f66730a2f2c3d7a67a724b954f56ee21ca
ZackRimshnick <zachary.rimshnick21@sitechhs.com>
2021-05-12 16:15:10 -0400

    some work

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,18 @@
-// import { DOMSelectors } from "./DOM";
+import { DOMSelectors } from "./DOM";
+import { genres } from "./genre";
+
+///// CODE /////
+
+const query = async function () {
+  try {
+    const response = await fetch();
+    // `https://api.themoviedb.org/3/discover/movie?sort_by=average_vote.asc&vote_count.gte=10000&vote_average.gte=8&api_key=${key}`
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+    alert("Hey something went wrong");
+  }
+};
+
+query();
