8e9b62c12e052154bb12beb92c784bb278222e62
ZackRimshnick <zachary.rimshnick21@sitechhs.com>
2021-05-15 23:18:04 -0400

    still dont like it

--- src/js/index.js
+++ src/js/index.js
@@ -2,20 +2,6 @@ import { DOMSelectors } from "./DOM";
 
 ///// CODE /////
 
-/* const query = async function () {
-  try {
-    const response = await fetch();
-    // `https://api.themoviedb.org/3/discover/movie?sort_by=average_vote.asc&vote_count.gte=10000&vote_average.gte=8&api_key=${key}`
-    const data = await response.json();
-    console.log(data);
-  } catch (error) {
-    console.log(error);
-    alert("something went wrong");
-  }
-};
-
-query(); */
-
 const listen = function () {
   DOMSelectors.searchForm.addEventListener("submit", function (e) {
     e.preventDefault();
