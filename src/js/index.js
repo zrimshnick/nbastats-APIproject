3e41455e499db4e381991868855d9c491ef8e457
ZackRimshnick <zachary.rimshnick21@sitechhs.com>
2021-05-17 22:30:30 -0400

    still not showing api info

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,28 @@
 import { DOMSelectors } from "./DOM";
 
+// reference
+//const searchAreaPlaceholder = document.getElementById("search-form").placeholder;
+const filterPlayer = document.getElementsByClassName("btn-player");
+const filterTeam = document.getElementsByClassName("btn-team");
+const filterStats = document.getElementsByClassName("btn-stats");
+
+// reference use
+filterPlayer.addEventListener("click", () => {
+  document.getElementById("search-area").placeholder = "Search for a player";
+  console.log("click working");
+});
+filterTeam.addEventListener("click", () => {
+  document.getElementById("search-area").placeholder = "Search for a team";
+});
+filterStats.addEventListener("click", () => {
+  //DOMSelectors.searchAreaPlaceholder.innerHTML(`textarea class="search-area"
+  //name="search-area"
+  //id="search-area"
+  //placeholder="search for a player for their stats"></textarea>`);
+  document.getElementById("search-area").placeholder =
+    "Search for a player for their stats";
+});
+
 ///// CODE /////
 
 const listen = function () {
@@ -20,9 +43,7 @@ const listen = function () {
           console.log(player.last_name);
           DOMSelectors.list.insertAdjacentHTML(
             "beforeend",
-            `<div class="player-row">
-              <div class="player-name">${player.last_name}</div>
-             </div>`
+            `<div class="player-name">${player.last_name}</div>`
           );
           console.log(player.data);
         });
